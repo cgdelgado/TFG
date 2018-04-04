eec7a4348b80770c827e7051bb35ea0741664196
cgdelgado <claugd2012@gmail.com>
2018-04-04 20:37:38 +0200

    funciona menu navbar responsive

--- Plantillas/p1/main1.js
+++ Plantillas/p1/main1.js
@@ -1,16 +1,9 @@
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}*/
 (function($) {
-  "use strict"; // Start of use strict
+  alert("Tengo js!!");
+ "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+/ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
